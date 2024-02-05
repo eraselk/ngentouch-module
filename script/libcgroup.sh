535ce45e7b46ac7d886497d2f5226099b4098634
eraselk <141639047+eraselk@users.noreply.github.com>
2024-02-05 09:25:02 +0800

    Simplify function #2

--- script/libcgroup.sh
+++ script/libcgroup.sh
@@ -2,6 +2,10 @@
 # Copyright eraselk 2023-2024
 
 change_task_cgroup() {
-  ps -e | grep -i -E "$1" | awk '{print $1}' | \
-  xargs -I{} sh -c 'for tid in /proc/{}/task/*; do echo "${tid##*/}" > "/dev/$3/$2/tasks"; done'
+  echo "$ps_ret" | grep -i -E "$1" | awk '{print $1}' | while read -r temp_pid; do
+    ls "/proc/$temp_pid/task/" | while read -r temp_tid; do
+      comm=$(cat "/proc/$temp_pid/task/$temp_tid/comm")
+      echo "$temp_tid" > "/dev/$3/$2/tasks"
+    done
+  done
 }
